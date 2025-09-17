4e99c4ccc034e2ae2f5ef3fce609f104d29ecd34
DigvijaySinghRajput <rajputdd15@gmail.com>
2025-09-17 23:37:05 +0530

    NodeMailerServiceAdded

--- src/helpers/sendVericationEmail.ts
+++ src/helpers/sendVericationEmail.ts
@@ -1,19 +1,42 @@
 import { resend } from "@/lib/resend";
 import resendVerifacationEmailTemplate from "../../react-email-starter/emails/resendVerificationEmailTemplate";
 import { ApiResponse } from "@/types/ApiResponse";
-
+import nodemailer from "nodemailer";
+import { render } from "@react-email/components";
 export async function sendverificationEmail(
   email: string,
   username: string,
   verifytoken: string
 ): Promise<ApiResponse> {
   try {
-    await resend.emails.send({
-      from: "rajputds@resend.dev",
-      to: email,
-      subject: "MystryWorld || Email Verification",
-      react: resendVerifacationEmailTemplate({ username, otp: verifytoken }),
+    // await resend.emails.send({
+    //   from: "rajputds@resend.dev",
+    //   to: email,
+    //   subject: "MystryWorld || Email Verification",
+    //   react: resendVerifacationEmailTemplate({ username, otp: verifytoken }),
+    // });
+    // return {
+    //   success: true,
+    //   message: "Verification email sent successfully",
+    // };
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_APP_PASSWORD,
+      },
     });
+
+    const htmlcontent = await render(
+      resendVerifacationEmailTemplate({ username, otp: verifytoken })
+    );
+    await transporter.sendMail({
+      from: `"MystryWorld" ${process.env.GMAIL_USER}`,
+      to: email,
+      subject: "MystryWorld || Verification Email",
+      html: htmlcontent,
+    });
+    console.log("Mail sent for verification");
     return {
       success: true,
       message: "Verification email sent successfully",
